03d4b955adba140fd91cd52df1c2b2373b90528b
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-10 16:17:38 +0200

    witching Auth Modes With Query Parameters (Search Params)

--- components/auth-form.js
+++ components/auth-form.js
@@ -2,10 +2,9 @@
 
 import Link from 'next/link';
 import  { useFormState } from 'react-dom';
-
 import { signup } from '@/actions/auth-actions';
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) { // 'login', 'signup'
 
  const [formState, formAction] =  useFormState(signup, {});
 
@@ -31,11 +30,12 @@ export default function AuthForm() {
         )}
       <p>
         <button type="submit">
-          Create Account
+          { mode === 'login' ? 'Login' : 'Create Account' }
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        { mode === 'login' && <Link href="/?mode=signup">Create an account.</Link> }
+        { mode === 'signup' && <Link href="/?mode=login">Login with existing account.</Link> }
       </p>
     </form>
   );
