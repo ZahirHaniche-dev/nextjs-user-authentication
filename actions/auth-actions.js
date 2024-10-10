e217c935be1f247909068a866b1e042dffd37575
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-10 12:15:59 +0200

    User Signup: Extracting & Validating User Input

---
+++ actions/auth-actions.js
@@ -0,0 +1,26 @@
+"use server";
+
+export async function signup(prevState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password'); 
+
+    let errors = {};
+
+    if(!email.includes('@')) {
+        errors.email = 'Please enter a valid email address';
+    }
+    
+    if(password.trim().length < 8) {
+        errors.password = 'Password must be at least 8 characters long';
+    }
+    
+    if(Object.keys(errors).length > 0) {
+        return { errors };
+    }
+
+    if(Object.keys(errors).length){
+        return { errors };
+    }
+
+    // store in the database (create a new user)
+}
